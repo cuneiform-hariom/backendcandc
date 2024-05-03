d6469f1f45cde667cd1317bf7c382ad4ca9cb64c
cuneiform-hariom <hariom.karn@thecuneiform.com>
2024-05-03 10:45:12 +0530

    setup done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-require("dotenv").config()
-const express = require('express')
+import express from "express"
+
 const app = express()
 const port = process.env.PORT
 
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
